a8f93044125aa95889325b06a4132df68121f055
Hercules Dias Campos <64754149+HerculesKHL@users.noreply.github.com>
2021-06-08 17:50:34 -0400

    Update scene.py and instructions.txt, add collectible.py

--- src/scene.py
+++ src/scene.py
@@ -1,6 +1,28 @@
+from pygame import Color, Rect, Surface
+
 class Scene():
     def __init__(self):
         self.started = False
+        self.area_info = (0, 0, 450, 450)
+        self.cell_amt = 18
+        self.back_colour = Color(255, 255, 255)
+        self.cell_colour = Color(245, 242, 222)
+
+        self.cell_size = (int(self.area_info[2]/self.cell_amt),
+                          int(self.area_info[3]/self.cell_amt))
+        self.play_rect = Rect(self.area_info)
+        self.play_surf = Surface(self.play_rect.size)
+        self.play_surf.fill(self.back_colour)
+
+            #checkerboard draw
+        from pygame.draw import rect
+        for x in range(self.cell_amt):
+            for y in range(self.cell_amt):
+                if (x%2==0)^(y%2==0):
+                    rect(self.play_surf, self.cell_colour,
+                         Rect((x*self.cell_size[0],
+                               y*self.cell_size[1]),
+                              self.cell_size))
 
     def start(self):
         self.started = True
@@ -10,4 +32,4 @@ class Scene():
         pass
 
     def render(self, target):
-        pass
+        target.blit(self.play_surf, self.play_rect)
